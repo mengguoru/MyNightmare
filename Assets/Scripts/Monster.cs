950c3d3711903560e5cb4024d50900112d5f6aeb
mengguoru <mengguoru0@gmail.com>
2016-11-16 18:01:52 +0800

    dd

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+    public float speed;
+    GameObject target;
+    public float closestDis;
+
+    // Use this for initialization
+    void Start () {
+        target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(target.transform.position, transform.position) > closestDis)
+        {
+            Vector3 offset = target.transform.position - transform.position;
+            transform.Translate(offset.normalized *speed* Time.deltaTime);
+        }
+    }
+}
