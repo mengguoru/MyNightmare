400bf15d08c11db57a13b8825535a92a4ffb7030
mengguoru <mengguoru0@gmail.com>
2016-11-16 17:45:18 +0800

    get input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+    public float speed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hori = CrossPlatformInputManager.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Debug.Log(hori);
+        float veri = CrossPlatformInputManager.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(hori, 0, veri);
+    }
+}
