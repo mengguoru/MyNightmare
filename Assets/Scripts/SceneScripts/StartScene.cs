25e886042580b41b712322fcee82c1673279ad2e
mengguoru <mengguoru0@gmail.com>
2016-11-18 21:49:01 +0800

    dd

---
+++ Assets/Scripts/SceneScripts/StartScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour {
+    void startGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+    void showRank()
+    {
+        SceneManager.LoadScene("rankingList");
+    }
+}
---
+++ Assets/Scripts/SceneScripts/rankingList.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class rankingList : MonoBehaviour {
+    public Text txt1;
+    public Text txt2;
+    public Text txt3;
+    // Use this for initialization
+    void Start () {
+        txt1.text += PlayerPrefs.GetFloat("first");
+        txt2.text += PlayerPrefs.GetFloat("second");
+        txt3.text += PlayerPrefs.GetFloat("third");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void returnMain()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
