aed39d0e20ef2bccec73505179c2710f84a6c8fe
mengguoru <mengguoru0@gmail.com>
2016-11-18 20:52:48 +0800

    uitime

---
+++ Assets/Scripts/UITime.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UITime : MonoBehaviour {
+    public float timer;
+    public Text timeTxt;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        showTime();
+	}
+
+    void showTime()
+    {
+        float minutes = Mathf.Floor(timer / 60);
+        float seconds = Mathf.Floor(timer % 60);
+        timeTxt.text = "Time " + minutes + ":" + seconds;
+    }
+}
